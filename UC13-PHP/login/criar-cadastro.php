e76f43c05b009a7f26fde864a123f5cf27789434
Paulo Henrique <ph744915@gmail.com>
2024-01-26 11:57:14 -0300

    cadastrar usuarios em tabelas relacionadas

--- UC13-PHP/login/criar-cadastro.php
+++ UC13-PHP/login/criar-cadastro.php
@@ -1,11 +1,27 @@
 <?php
-echo '<h2>criar-cadastro.php</h2>';
+//$nome = $_GET['nome']; //$_GET capta informações do tipo get
 
-$nome = $_POST['nome'];
+$nome = $_POST['nome']; //$_POST capta infomações do tipo post
 $email = $_POST['email'];
+
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
+function CadastroUsuarios($usuario, $senha)
+{
 
+    $conexao = new PDO("mysql:host=localhost;dbname=db_login", "root", "");
+    $query = "INSERT INTO 
+                tb_usuarios (usuario, senha) 
+            VALUE 
+                ('" . $usuario . "', '" . $senha . "')";
+    $conexao->exec($query);
 
+    $ultimo_id =  $conexao->lastInsertId();
+    return $ultimo_id;
+}
+
+$id_usuario = CadastroUsuarios($usuario, $senha);
+
+CadastroInformacoes($nome, $email, $id_usuario);
 
---
+++ UC13-PHP/login/form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="qualquer.php" method="GET">
+        <label for="id_usuario">nome...</label>
+        <input type="text"  id="id_usuario" name="id_usuario" 
+            class="" placeholder="aqui eu tenho um placeholder"
+           
+        >
+        <br>
+        <label for="tipos">tipos....</label>
+        
+        <input type="reset" value="limpar2">
+        <button type="submit">Aquii</button>
+        <button type="reset">limpar</button>
+
+    </form>
+</body>
+</html>
